a87ee8b7eb62c79fbd04d103dbb959e73e35fa59
darknightmare <rubentkd98@gmail.com>
2015-10-10 16:43:39 +0200

    drivers/base: Add BQ changes for piccolo

--- drivers/base/regmap/regmap-debugfs.c
+++ drivers/base/regmap/regmap-debugfs.c
@@ -145,7 +145,7 @@ static unsigned int regmap_debugfs_get_dump_start(struct regmap *map,
 			reg_offset = fpos_offset / map->debugfs_tot_len;
 			*pos = c->min + (reg_offset * map->debugfs_tot_len);
 			mutex_unlock(&map->cache_lock);
-			return c->base_reg + (reg_offset * map->reg_stride);
+			return c->base_reg + reg_offset;
 		}
 
 		*pos = c->max;
@@ -248,7 +248,11 @@ static ssize_t regmap_map_read_file(struct file *file, char __user *user_buf,
 				   count, ppos);
 }
 
+#if defined(CONFIG_AUDIO_CODEC_WM8998)	 || defined(CONFIG_AUDIO_CODEC_FLORIDA)// xuke @ 20150114	Enable debug.
+#define REGMAP_ALLOW_WRITE_DEBUGFS
+#else
 #undef REGMAP_ALLOW_WRITE_DEBUGFS
+#endif
 #ifdef REGMAP_ALLOW_WRITE_DEBUGFS
 /*
  * This can be dangerous especially when we have clients such as
